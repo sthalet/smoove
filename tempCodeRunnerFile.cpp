295ab6eac033d5ce0c0ebf3e90d2dde73c4769f1
yigit mert <ygtdmr1011@gmail.com>
2024-10-23 19:55:36 +0300

    modified:   calc+.cpp 	new file:   calc+.exe 	new file:   calcdouble.exe 	new file:   tempCodeRunnerFile.cpp 	new file:   tempCodeRunnerFile.exe

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -4,6 +4,6 @@ int main(){
     int a,b;
     std::cout<<"enter 2 numbers";
     std::cin>>a>>b;
-    std::cout<<"your numbers are "<<a<<" "<<b;
+    std::cout<<"your numbers are "<<a,b;
     return 0;
 }
