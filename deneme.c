724c80e2d0050fffa20b166632e0ad3474eae45e
yigit mert <ygtdmr1011@gmail.com>
2024-11-01 12:50:28 +0300

    new file:   deneme.c 	modified:   .vscode/settings.json 	modified:   switchGrading.java 	modified:   tempCodeRunnerFile.cpp

---
+++ deneme.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    float anne, baba, hedefboy;
+    char cins;
+    scanf("%f%f%s", &anne, &baba, &cins);
+    if (cins=='e'||cins=='E') {
+        hedefboy= (baba+anne-13)/2;
+        if(hedefboy<175) {
+            printf("turkiye ortalamasindan kısa olacak");
+        }
+        else {
+            printf("turkiye ortalamasindan uzun olacak");
+        }
+    }
+    else if(cins=='k'||cins=='K'){
+        hedefboy=(baba+anne+13)/2;
+        if (hedefboy<163) {
+            printf("turkiye ortalamasindan kısa olacak");
+        }
+        else {
+            printf("turkiye ortalamasindan uzun olacak");
+        }
+    }
+    else {
+        printf("gecerli bir boy giriniz");
+    }
+    printf("hedef boy = %0.2f",hedefboy);
+
+
+
+
+    return 0;
+}
