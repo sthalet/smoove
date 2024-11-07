7d180e1bb5d0bc08130b62fb42adf3cc5c602a67
yigit mert <ygtdmr1011@gmail.com>
2024-11-07 20:29:16 +0300

    emre destekli yazılım

---
+++ cKitap8_26.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int araba=1;
+    int max = 0;
+    int count = 0;
+    while(araba != 0){
+        printf("Giren arabanin modelini giriniz");
+        scanf("%d",&araba);
+        if (!((araba<10000) && (araba>999))){
+            printf("4 haneli bir sayi giriniz.");
+            continue;
+        }
+        if(max<araba){
+            max = araba;
+        }
+        if (max==araba){
+            count++;
+        }
+    }
+    printf("\nen yeni model : %d , bu modelden giren araba sayisi: %d",max,count);
+    return 0;
+}
